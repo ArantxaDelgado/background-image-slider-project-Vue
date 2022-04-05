4667b3ea9190920e9d67f5ee01ee0db83d0552b7
ardelgad <95305208+ardelgad@users.noreply.github.com>
2022-04-05 20:54:45 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const pictures = [
+  "contBcg-0",
+  "contBcg-1",
+  "contBcg-2",
+  "contBcg-3",
+  "contBcg-4"
+];
+
+Vue.createApp({
+  data() {
+    return {
+      image: "contBcg-0",
+      index: 0
+    }
+  },
+  methods: {
+    slider(value) {
+      this.index += value;
+
+      if (this.index < 0) {
+        this.index = pictures.length - 1;
+      } 
+      
+      else if (this.index >= pictures.length) {
+        this.index = 0;
+      }
+
+      this.image = pictures[this.index];
+    }
+  }
+}).mount("#app");
